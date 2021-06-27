eb70bd148abd2e352421d4f75ea8c3daafde567f
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-06-27 11:00:55 -0500

    Creando objeto nuevo a partir del accionar del boton Agregra al Carrito

--- js/app.js
+++ js/app.js
@@ -16,8 +16,27 @@ function cargarEventListeners() {
 function agregarCurso(e) {
     e.preventDefault();
     if(e.target.classList.contains('agregar-carrito')) {
-        console.log('Agregando');
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
     }
 }
+
+// Lee el contedido del html ya capturado
+
+function leerDatosCurso(curso) {
+    // Creando un nuevo objeto a partir de la info capturada
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+    console.log(infoCurso);
+}
+
+
+
+
 
 
