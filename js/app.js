9a3b6bd48c79ec859de31f4129b6cb3c371a181f
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-06-27 13:26:17 -0500

    Estilizando area de carrito de compras con las propiedades del objeto y aplicando destructuring para mejorar codigo

--- js/app.js
+++ js/app.js
@@ -47,12 +47,23 @@ function carritoHTML() {
 
 
     articulosCarrito.forEach(curso => {
+        const {imagen, titulo, precio, cantidad, id} = curso;
         const row = document.createElement('tr');
         row.innerHTML = `
             <td>
-
-                ${curso.titulo}
-
+                <img src="${imagen}" width="100">
+            </td>
+            <td>
+                ${titulo}
+            </td>
+            <td>
+                ${precio}
+            </td>
+            <td>
+                ${cantidad}
+            </td>
+            <td>
+                <a href="#" class='borrar-curso' data-id='${id}'> X </a>
             </td>
         `;
 
